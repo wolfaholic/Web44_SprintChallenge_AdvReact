65f93b06cb8c4e092ab1f083c5ba99a21c4412da
Melissa <wolfaholic@users.noreply.github.com>
2021-07-09 01:17:17 -0700

    Setting up branch

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import "./App.css";
 
 function App() {
   // array of plants that have been added to the cart
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState([]); 
 
   // add a plant to the cart
   const addToCart = (plant) => {
